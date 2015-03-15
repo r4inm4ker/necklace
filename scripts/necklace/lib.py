bee90105c5cf691821091fad88bd45621adac22b
rain_local <jefri.yeh@outlook.com>
2015-03-15 23:00:07 +0800

    move test scripts outside

--- scripts/necklace/lib.py
+++ scripts/necklace/lib.py
@@ -1,9 +1,14 @@
 import pymel.core as pm
 
+def loadPlugin():
+    if not pm.pluginInfo("necklaceNode",q=1,loaded=True):
+        pm.loadPlugin("necklaceNode.py")
+
 def attachNecklace(curveNode):
     curveNode = pm.PyNode(curveNode)
     curveNode.scale.set(4,4,4)
 
+    loadPlugin()
     necklaceNode = pm.createNode('necklace')
 
     curveNode.worldSpace[0] >> necklaceNode.inputCurve
--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,10 @@
 import pymel.core as pm
 import necklace.lib as nlib
 
-
 def create():
+    pm.mel.eval('file -f -new;')
+    pm.importFile('E:/git/brainyart/necklace/scenes/axisgeo.ma', namespace='axisgeo1')
+
     curve = pm.circle()[0]
 
     necklaceNode = nlib.attachNecklace(curve)
@@ -13,7 +15,7 @@ def create():
     necklaceNode.upVector.set(0,0,1)
 
     for idx in range(numSample):
-        geo = pm.duplicate('axisgeo')[0]
+        geo = pm.duplicate('axisgeo1:axisgeo')[0]
         pm.connectAttr(necklaceNode.outPosition[idx].outPosition,geo.translate)
         pm.connectAttr(necklaceNode.outRotation[idx].outRotation,geo.rotate)
   
